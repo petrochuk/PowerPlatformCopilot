d013fa5325156f358610350da0e3f4a8cb7e8b5a
Andrew Petrochuk <anpetroc@microsoft.com>
2023-06-10 19:04:32 -0700

    Fixed multiple RE

--- src/PacAI/DataverseCopilot/Graph/GraphExtensions.cs
+++ src/PacAI/DataverseCopilot/Graph/GraphExtensions.cs
@@ -7,6 +7,16 @@ internal static class GraphExtensions
         if (string.IsNullOrWhiteSpace(subject))
             return subject;
 
-        return subject.Replace("RE: ", string.Empty, StringComparison.OrdinalIgnoreCase);
+        while (true)
+        {
+            if(subject.StartsWith("RE: ", StringComparison.OrdinalIgnoreCase))
+                subject = subject.Substring(4);
+            else if (subject.StartsWith("FW: ", StringComparison.OrdinalIgnoreCase))
+                subject = subject.Substring(4);
+            else
+                break;
+        }
+
+        return subject;
     }
 }
